68a2f64deccdbc68ae56775885fa8658424b4324
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-05-24 12:19:09 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -35,7 +35,7 @@
 #define HIST_FILE	".somple_shell_history"
 #define HIST_MAX	4096
 
-exter char **environ;
+extern char **environ;
 
 /**
  * struct liststr - singly linked list
@@ -122,7 +122,7 @@ void fork_cmd(info_t *);
 /* parser.c */
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
-char *find_path(info *, char *, char *);
+char *find_path(info_t *info, char *, char *);
 
 /*loopsh.c*/
 int loophsh(char **);
@@ -137,7 +137,7 @@ int _putsfd(char *str, int fd);
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
+char *_strcati(char *, char *);
 
 /* string1.c */
 char *_strcpy(char *, char *);
@@ -156,7 +156,7 @@ char **strtwo2(char *, char);
 
 /* realloc.c */
 char *_memset(char *, char, unsigned int);
-void s_free(char **);
+void s_free(char *);
 void *_realloc(void *, unsigned int, unsigned int);
 
 /* memory.c */
@@ -187,9 +187,11 @@ int _myalias(info_t *);
 /* toem_getline.c */
 ssize_t get_input_buffer(info_t *);
 ssize_t getline_input(info_t *);
-ssize_t read_buffer(info_t *, size_t *, **);
+ssize_t read_buffer(info_t *, size_t *, *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(__attribute__((unused))int);
+void clear_info(info_t *);
+void set_info(info_t *, char**);
 
 /* toem_getlininfo.c */
 void init_info_struct(info_t *);
@@ -198,7 +200,7 @@ void free_info_struct(info_t *, int);
 
 /* toem_getenv.c*/
 char **get_environ_info(info_t *);
-int get_unsetenv(info_t *, char *)
+int get_unsetenv(info_t *, char *);
 int get_setenv(info_t *, char *, char *);
 
 /* toem_environ.c */
@@ -212,21 +214,21 @@ int populate_env_list(info_t *);
 char *checks_history_file(info_t *info);
 int create_history(info_t *info);
 int read_history_file(info_t *info);
-int list_history(info_t *, char *, int);
+int list_history(info_t *info, char *, int);
 int reassign_history(info_t *);
 
 /* toem_lists.c */
 list_t *add_node_start(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list(const list_t *);
-int delete_node_index(list_t **, unsigned int);
+int delete_node_idx(list_t **, unsigned int);
 void free_list_all(list_t **);
 
 /* toem_lists2.c */
-size list_length_linked(const list_t *);
+size_t list_length_linked(const list_t *);
 char **list_strings(list_t *);
 size_t print_list_all(const list_t *);
-list_t *node_pointer(info_t *, char , char);
+list_t *node_pointer(info_t *, char, char);
 ssize_t get_node(list_t *, list_t *);
 
 /* toem_vars.c */
@@ -234,6 +236,6 @@ int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
 int replace_vars(info_t *);
-int replace string(char **, char *);
+int replace _string(char **, char *);
 
 #endif
